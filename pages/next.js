5cc3d24a0f9eed559465fb7967f90426151b2baa
abdallah_void <abdallah_void@Aehams-MacBook-Pro.local>
2022-03-25 08:34:04 +0100

    add next education

---
+++ pages/next.js
@@ -0,0 +1,14 @@
+
+import Head from "next/head";
+
+import Container from "../components/container";
+import Layout from "../components/layout";
+
+export default function Next() {
+
+    return (
+        <div className='overflow-y-hidden'>
+            Next education
+        </div>
+    )
+}
